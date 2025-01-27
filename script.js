6eef43cb88fe84d153569c0391bcaac908b3d9cd
irvingarin4395 <irvingarin4395@gmail.com>
2025-01-27 10:54:47 +0800

    fixed mobile view

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
     $("#contact_btn").click(function(){
         $("#contact_modal").css("display", "flex");
     });
+    $('.exp_card_front[company="sapere_marketing"]').click(function(){
+        $('.exp_card_front[company="sapere_marketing"]').css("opacity", "0");
+        $('.exp_card_back[company="sapere_marketing"]').css("transform", "scale(1)");
+        $('.exp_card_layer[company="sapere_marketing"]').css("transform", "scale(1)");
+    });
+    $('.exp_card_back[company="sapere_marketing"]').click(function(){
+        $('.exp_card_front[company="sapere_marketing"]').css("opacity", "1");
+        $('.exp_card_back[company="sapere_marketing"]').css("transform", "scale(0)");
+        $('.exp_card_layer[company="sapere_marketing"]').css("transform", "scale(0)");
+    });
+    $('.exp_card_front[company="udd"]').click(function(){
+        $('.exp_card_front[company="udd"]').css("opacity", "0");
+        $('.exp_card_back[company="udd"]').css("transform", "scale(1)");
+        $('.exp_card_layer[company="udd"]').css("transform", "scale(1)");
+    });
+    $('.exp_card_back[company="udd"]').click(function(){
+        $('.exp_card_front[company="udd"]').css("opacity", "1");
+        $('.exp_card_back[company="udd"]').css("transform", "scale(0)");
+        $('.exp_card_layer[company="udd"]').css("transform", "scale(0)");
+    });
 });
